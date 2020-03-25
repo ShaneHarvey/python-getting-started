7601b9dd5c395e3f7b7cd40c96228abdd8675046
Shane Harvey <shnhrv@gmail.com>
2020-03-25 16:49:43 -0700

    Add memory_profiler output

--- hello/views.py
+++ hello/views.py
@@ -9,6 +9,11 @@ from bson.son import SON
 from bson.objectid import ObjectId
 
 import psutil
+import memory_profiler
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 host_id = ObjectId()
 
@@ -34,24 +39,29 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 
-def mongodb(request):
+def _get_documents(request):
     uri = os.environ.get('MONGODB_URI')
     if not uri:
-        return render(request, "mongodb.html", {"documents": ['MONGODB_URI not set!']})
+        return ['MONGODB_URI not set!']
 
     limit = 10
     with MongoClient(uri) as client:
         coll = client.heroku.test
         for i in range(limit):
-            coll.insert_one(SON([
-                ('mem', get_mem()),
-                ('units', 'bytes'),
-                ('client', i),
-                ('host', host_id)]))
+            coll.insert_one(SON([('mem', get_mem()), ('units', 'bytes'),
+                                 ('client', i), ('host', host_id)]))
             # Close all connections.
             client.close()
         documents = list(coll.find(
             {'host': host_id}, limit=limit*50, sort=[('_id', -1)],
             projection={'_id': False, 'host': False}))
 
-    return render(request, "mongodb.html", {"documents": documents})
+    return documents
+
+
+def mongodb(request):
+    stream = StringIO()
+    docs = memory_profiler.profile(_get_documents, stream=stream)(request)
+    extra = stream.getvalue()
+
+    return render(request, "mongodb.html", {"documents": docs, "extra": extra})
